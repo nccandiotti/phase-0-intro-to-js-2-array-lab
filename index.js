fff40ff0f30c04c96db9fe25d5d645e870fa89e1
Nicole Candiotti <nccandiotti@gmail.com>
2022-01-11 22:05:31 -0500

    commit tree 0c3f79eff960400f3acf2b65adf1538042c36dda

--- index.js
+++ index.js
@@ -1,34 +1,4 @@
 const cats = ["Milo", "Otis", "Garfield"];
-
-function destructivelyAppendCat(Ralph) {
-    return cats.push("Ralph");
+function destructivelyAppendCat() {
+  return cats.push("Ralph");
 }
-function destructivelyPrependCat(Bob) {
-    return cats.unshift("Bob");
-}
-function destructivelyRemoveFirstCat(){
-    return cats.shift();
-}
-function destructivelyRemoveLastCat(){
-    return cats.pop();
-}
-
-function removeLastCat() {
-    return cats.splice[2];
-};
-
-function removeFirstCat() {
-    return cats.splice[0];
-};
-function prependCat(Arnold) {
-    return ["Arnold", ...cats];
-  };
-function appendCat(Broom) {
-    return [...cats, "Broom"];
-};
-function removeLastCat () {
-    return cats.slice(0, cats.length -1);
-};
-function removeFirstCat () {
-    return cats.slice(1);
-};
